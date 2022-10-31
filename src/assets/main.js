27d8e2d1e0aa45da2935481b3ec7c86623ac7730
osdan97 <daniejurade@gmail.com>
2022-10-31 01:05:44 -0500

    1er push

---
+++ src/assets/main.js
@@ -0,0 +1,42 @@
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCUu2wCrHdEQAiUlLVgA5VUA&part=snippet%2Cid&order=date&maxResults=9';
+
+const content = null || document.getElementById('content');
+
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+    'X-RapidAPI-Key': 'da08ade2bbmsha1a97842ae51700p1782eejsnbd2aed5333e2'
+  }
+};
+
+async function fetchData(urlApi) {
+  const response = await fetch(urlApi, options);
+  const data = await response.json();
+  return data;
+}
+
+(async () => {
+  try {
+    const videos = await fetchData(API);
+    let view = `
+    ${videos.items.map(video => `
+      <div class="group relative">
+        <div
+          class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+        </div>
+        <div class="mt-4 flex justify-between">
+          <h3 class="text-sm text-gray-700">
+            <span aria-hidden="true" class="absolute inset-0"></span>
+            ${video.snippet.title}
+          </h3>
+        </div>
+      </div>
+    `).slice(0, 7).join('')}
+    `;
+    content.innerHTML = view;
+  } catch (error) {
+    console.log(error);
+  }
+})();
